af6d482788cb6a30977115804282e1b5d1f3ea77
AnaLara <analara714@gmail.com>
2025-02-10 10:02:21 -0300

    atualiza o diagrama, cria o DB e o arquivo de povoamento do DB

---
+++ povoar_BD.sql
@@ -0,0 +1,33 @@
+use `Equipe498866`;
+
+/*INSERT INTO `agencia`(`num_ag`, `nome_ag`, `cidade`, `sal_total`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");
+  
+INSERT INTO `cliente`(`cpf`, `nome_comp`, `rg`, `orgao_emissor`, `uf`, `data_nasc`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");
+                     
+INSERT INTO `cliente_has_conta`(`cliente_cpf`, `conta_num_conta`, `agencia_num_ag`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");  
+
+INSERT INTO `conta`(`num_conta`, `saldo`, `senha`, `tipo_conta`, `juros`, `limite_credito`, `data_aniversário`, `gerente_matricula`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");
+
+INSERT INTO `dependente`(`id_dep`, `matricula`, `nome_comp`, `data_nas`, `idade`, `parentesco`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");
+                     
+INSERT INTO `email_cliente`(`id_email`, `cliente_cpf`, `email`, `tipo_email`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");
+
+INSERT INTO `endereco`(`id_end`, `cliente_cpf`, `tipo_log`, `rua`, `num`, `bairro`, `cep`, `cidade`, `estado`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");					
+
+INSERT INTO `funcionario`(`matricula`, `num_ag`, `nome_comp`, `senha`, `senha`, `endereço`, `cidade`, `cargo`, `genero`, `data_nasc`, `salario`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");
+
+INSERT INTO `telefones_cliente`(`id_tel`, `cliente_cpf`, `telefone`, `tipo_tel`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");
+
+INSERT INTO `transacao`(`num_transacao`, `conta_num_conta`, `tipo_transacao`, `data_hora`, `valor`) VALUES 
+					 (NULL,  "Cras Corporation", "Sobral","40000000000");
+*/
+					
